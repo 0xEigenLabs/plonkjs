8b1df92b0c6ad7816baf62eede86e2a46c3b9fed
eigmax <stephen@ieigen.com>
2023-04-29 12:54:13 +0800

    refactor: error handle #1

--- tests/secret.test.js
+++ tests/secret.test.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const { prover } = require("../");
 
 describe.skip("Secret prove and verify test", function() {
-  // this.timeout(5000000);
+  this.timeout(5000000);
   it("Should return true when proof is correct", async function() {
     // let basePath = __dirname;
     let basePath = "/tmp/secret";
@@ -15,6 +15,10 @@ describe.skip("Secret prove and verify test", function() {
     let wtns_content = fs.readFileSync(wtns);
     let srs_monomial_form = path.join(basePath, "setup_2^18.key");
     let srs_monomial_form_content = fs.readFileSync(srs_monomial_form);
+
+    let start = new Date().getTime();
+    console.log(start);
+
     let proof = prover.prove(
       circuit_file_content.toJSON().data,
       wtns_content.toJSON().data,
@@ -22,11 +26,16 @@ describe.skip("Secret prove and verify test", function() {
       "keccak"
     );
 
+    let end = new Date().getTime();
+    console.log(`prove cost: ${(end - start)/1000} s`);
     // generate verify key
     let vk = prover.export_verification_key(
       srs_monomial_form_content.toJSON().data,
       circuit_file_content.toJSON().data
     );
+
+    let end2 = new Date().getTime();
+    console.log(`export vk cost: ${(end2 - end)/1000} s`);
 
     // verify
     let verify_ok = prover.verify(
@@ -34,7 +43,8 @@ describe.skip("Secret prove and verify test", function() {
       Array.from(proof.proof_bin),
       "keccak"
     )
-    console.log(verify_ok);
+    let end3 = new Date().getTime();
+    console.log(`verify cost: ${(end3 - end2)/1000} s`);
     expect(verify_ok).eq(true)
   });
 });
